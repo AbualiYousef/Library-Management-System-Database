83cb750f4e7b50aaada84c537ea2c19fb1543194
Yousef Abuali <abualiyousef@outlook.com>
2024-04-18 22:56:34 +0200

    Add 'Stored Procedure - Add New Borrowers' solution script

---
+++ complexQueriesAndProcedures/5.StoredProcedure-AddNewBorrowers.sql
@@ -0,0 +1,28 @@
+--5.Stored Procedure - Add New Borrowers:
+CREATE PROCEDURE sp_AddNewBorrower
+    @FirstName VARCHAR(255),
+    @LastName VARCHAR(255),
+    @Email VARCHAR(255),
+    @DateOfBirth DATE,
+    @MembershipDate DATE
+AS
+BEGIN
+    IF EXISTS (SELECT Borrowers.borrowerId FROM Borrowers WHERE email = @Email)
+    BEGIN
+        SELECT 'Error: A borrower with this email already exists.' AS ErrorMessage;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO Borrowers
+        VALUES (@FirstName, @LastName, @Email, @DateOfBirth, @MembershipDate);
+        SELECT SCOPE_IDENTITY() AS NewBorrowerID;
+    END
+END;
+
+-- Test the procedure by executing it
+EXEC sp_AddNewBorrower 
+    @FirstName = 'John',
+    @LastName = 'Doe',
+    @Email = 'john.doe@example.com',
+    @DateOfBirth = '1990-05-15',
+    @MembershipDate = '2023-04-15';
